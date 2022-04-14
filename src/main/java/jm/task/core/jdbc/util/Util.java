9860f100abc77e0d1ba862c59b839cafecf8354e
WoWheaded <Swimming232!>
2022-04-14 20:30:10 +0300

    1st update 1_1_4

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -34,13 +34,13 @@ public class Util {
                 Configuration configuration = new Configuration();
                 Properties properties = new Properties();
                 properties.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
-                properties.put(Environment.URL, "jdbc:mysql://localhost:3306/pp1_1_3?useSSL=false&allowMultiQueries=true&serverTimezone=UTC");
+                properties.put(Environment.URL, "jdbc:mysql://localhost:3306/pp1_1_3?useLegacyDatetimeCode=false&serverTimezone=Europe/Moscow");
                 properties.put(Environment.USER, USERNAME);
                 properties.put(Environment.PASS, PASSWORD);
                 properties.put(Environment.DIALECT, "org.hibernate.dialect.MySQL8Dialect");
                 properties.put(Environment.SHOW_SQL, "true");
                 properties.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
-                properties.put(Environment.HBM2DDL_AUTO, "create-drop");
+                properties.put(Environment.HBM2DDL_AUTO, "update");
                 configuration.setProperties(properties);
                 configuration.addAnnotatedClass(User.class);
 
